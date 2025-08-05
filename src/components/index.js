d1b01dcafc4df2b0daa7b4c0485a493e8c264aa8
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-08-05 17:18:06 +0530

    feat: add reusable components including Button, Logo, PostCard, Select, Container, Footer, Header, LogoutBtn, and Input

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,12 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import Container from './container/Container'
+import LogoutBtn from "./header/LogoutBtn";
 
-export {Header, Footer}
+export {Header,
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn
+}
