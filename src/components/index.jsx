fcbf122973e1ccdb04f845c4bf0abc0a526949ff
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-08-22 02:55:50 +0530

    change index file extension name

---
+++ src/components/index.jsx
@@ -0,0 +1,32 @@
+import Header from "./header/Header.jsx";
+import Footer from "./footer/Footer.jsx";
+import Logo from "./Logo.jsx";
+import Container from './container/Container.jsx'
+import LogoutBtn from "./header/LogoutBtn.jsx";
+import Input from "./Input.jsx";
+import Button from "./Button.jsx"
+import RTE from "./RTE.jsx";
+import Select from "./Select.jsx";
+import Signup from './Signup.jsx'
+import PostCard from './PostCard.jsx'
+import Login from './Login.jsx'
+import PostForm from "./postForms/PostForm.jsx";
+import AuthLayout from './AuthLayout.jsx';
+
+export {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    Input,
+    LogoutBtn,
+    Button,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+
+}
