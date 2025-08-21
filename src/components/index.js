118e9fcb131f4b42a78240c24693634b29b65bbd
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-08-22 02:53:02 +0530

    fix: Correct import path for Input component

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Container from './container/Container'
 import LogoutBtn from "./header/LogoutBtn";
-import Input from "./Input";
+import Input from "./Input.jsx";
 import Button from "./Button"
 import RTE from "./RTE";
 import Select from "./Select";
